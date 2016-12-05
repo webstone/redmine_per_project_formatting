cd496c8f479f9f2495bc341c53e43ae39fa2ab81
Gregory Van der Steen <gregory@webstone.be>
2016-12-05 20:47:23 +0100

    Add myself to authors list.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ end
 
 Redmine::Plugin.register :redmine_per_project_formatting do
   name 'Redmine per project formatting plugin'
-  author 'Akihiro Ono'
+  author 'Akihiro Ono, Gregory Van der Steen'
   description 'Redmine plugin for per-project text formatting'
   version '0.0.4'
   url 'https://github.com/a-ono/redmine_per_project_formatting'
