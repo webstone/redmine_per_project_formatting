9ccd42004ed2f4f0d8e4fb2bcaf69ea646d098fb
Gregory Van der Steen <gregory@webstone.be>
2017-09-08 13:53:06 +0200

    Fix application helper patch module/namespace. Other applicationhelper overrides were no longer loaded.

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 require 'redmine'
 require 'redmine_per_project_formatting'
 
-Rails.application.config.to_prepare do
-  RedminePerProjectFormatting.apply_patch
-end
-
 Redmine::Plugin.register :redmine_per_project_formatting do
   name 'Redmine Per Project Formatting plugin'
   author 'Akihiro Ono, Gregory Van der Steen'
   description 'Redmine plugin for per-project text formatting'
   version '0.0.4'
   url 'https://github.com/a-ono/redmine_per_project_formatting'
+end
+
+ActionDispatch::Callbacks.to_prepare do
+  RedminePerProjectFormatting.apply_patch
 end
--- lib/redmine_per_project_formatting/application_helper_patch.rb
+++ lib/redmine_per_project_formatting/application_helper_patch.rb
@@ -1,22 +1,38 @@
-require 'application_helper'
-module ApplicationHelper
-  def textilizable_with_per_project(*args)
-    options = args.last.is_a?(Hash) ? args.last : {}
-    obj = options[:object] || args.first 
-    project = options[:project] || @project || if obj.is_a?(Project)
-      obj
-    elsif obj.respond_to?(:project)
-      obj.project
+require_dependency 'application_helper'
+
+module RedminePerProjectFormatting
+  module ApplicationHelperPatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
+
+      base.class_eval do
+        unloadable
+        alias_method_chain :textilizable, :per_project
+      end
     end
 
-    if project.try(:project_wide_formatting) && Setting.current_text_formatting.nil?
-      Setting.within_text_formatting(project.text_formatting) do
-        textilizable_without_per_project(*args)
+    module InstanceMethods
+      def textilizable_with_per_project(*args)
+        options = args.last.is_a?(Hash) ? args.last : {}
+        obj = options[:object] || args.first
+        project = options[:project] || @project || if obj.is_a?(Project)
+          obj
+        elsif obj.respond_to?(:project)
+          obj.project
+        end
+
+        if project.try(:project_wide_formatting) && Setting.current_text_formatting.nil?
+          Setting.within_text_formatting(project.text_formatting) do
+            textilizable_without_per_project(*args)
+          end
+        else
+          textilizable_without_per_project(*args)
+        end
       end
-    else
-      textilizable_without_per_project(*args)
     end
   end
+end
 
-  alias_method_chain :textilizable, :per_project
+unless ApplicationHelper.included_modules.include?(RedminePerProjectFormatting::ApplicationHelperPatch)
+  ApplicationHelper.send(:include, RedminePerProjectFormatting::ApplicationHelperPatch)
 end
